34188d8eae8ef2517f2e300bcb7c555d6879a6b4
Korolev Evgen <fallenaking@gmail.com>
2023-12-04 17:46:07 +0200

    Swr 11513 v3 (#5)
    * SWR-11513-v3. added service provider for v3.0
    
    * SWR-11513-v3. added service provider for v3.0
    
    ---------
    
    Co-authored-by: korolev <ykorolov@neklo.com>

---
+++ src/Monolog/LoggerServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Salesmessage\Monolog;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Log\Logger;
+
+class LoggerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (getenv('LOG_SALESMSG_FORMATTER_ENABLED')) {
+            $salesmessageLogger = new SalesmessageLogger();
+            $salesmessageLogger($this->app->get(Logger::class));
+        }
+    }
+}
